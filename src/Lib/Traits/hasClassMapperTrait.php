ac162d4ddd26471fc5a80e41238b68d8cebb5651
Alistair Kearney <hi@alistairkearney.com>
2016-06-21 09:41:00 +1000

    Added hasClassMapper trait used by custom class map objects

---
+++ src/Lib/Traits/hasClassMapperTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Symphony\ClassMapper\Lib\Traits;
+
+/**
+ * This trait contains the mandatory member variables used by AbstractClassMapper
+ */
+trait hasClassMapperTrait
+{
+    protected static $sectionFields;
+    protected static $fieldMapping = [];
+    protected static $section = null;
+}
