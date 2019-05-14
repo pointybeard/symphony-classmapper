a83768928ad881fda14a39580fd9dac12642cbc6
Alannah Kearney <hi@alannahkearney.com>
2019-05-14 18:29:13 +1000

    Removed OPERATOR_OR and OPERATOR_AND contants from FilterInterface since they are provided by AbstractFilter

--- src/ClassMapper/Interfaces/FilterInterface.php
+++ src/ClassMapper/Interfaces/FilterInterface.php
@@ -4,9 +4,6 @@ namespace Symphony\ClassMapper\ClassMapper\Interfaces;
 
 interface FilterInterface
 {
-    const OPERATOR_OR = "OR";
-    const OPERATOR_AND = "AND";
-
     public function operator() : string;
     public function toArray() : array;
     public function pattern($includeOperator=true) : string;
