cc01374907cecdf11d9a136c0bce8c418ff3e784
Alannah Kearney <hi@alannahkearney.com>
2019-05-15 21:12:17 +1000

    Added AbstractSortableModel and updated SortableModelInterface

---
+++ src/ClassMapper/AbstractSortableModel.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Symphony\ClassMapper\ClassMapper;
+
+use Symphony\ClassMapper\ClassMapper\Exceptions;
+use pointybeard\Helpers\Functions\Flags;
+
+abstract class AbstractSortableModel extends AbstractModel implements Interfaces\SortableModelInterface
+{
+
+    protected static $fetchSqlTemplate = "SELECT SQL_CALC_FOUND_ROWS e.id as `id`, %s
+        FROM `tbl_entries` AS `e` %s
+        WHERE e.section_id = %d AND %s
+        GROUP BY e.id ORDER BY %%s.%%s %%s
+    ";
+
+    private static function findSortField() : ?string
+    {
+        foreach (static::getCustomFieldMapping() as $field => $mapping) {
+            if (isset($mapping['flags']) && Flags\is_flag_set($mapping['flags'], self::FLAG_SORTBY)) {
+                return $field;
+            }
+        }
+
+        return null;
+    }
+
+    private static function findSortDirection(?string $sortByField) : string
+    {
+        if (is_null($sortByField)) {
+            $sortByField = self::findSortField();
+        }
+
+        $fieldMappings = static::getCustomFieldMapping();
+        $direction = self::SORT_DIRECTION_ASC;
+
+        if (isset($fieldMappings[$sortByField]['flags']) && Flags\is_flag_set($fieldMappings[$sortByField]['flags'], self::FLAG_SORTDESC)) {
+            $direction = self::SORT_DIRECTION_DESC;
+        }
+
+        return $direction;
+    }
+
+    protected static function fetchSQL(string $where = null) : string
+    {
+
+        // Default is to sort by entry id
+        $join = 'e';
+        $column = 'id';
+
+        if(null == ($sortBy = self::findSortField())) {
+            $join = self::findJoinTableFieldName($field);
+            $column = self::findDatabaseFieldName($field);
+        }
+
+        return sprintf(
+            parent::fetchSQL($where),
+            $join,
+            $column,
+            self::findSortDirection($sortBy)
+        );
+    }
+}
--- src/ClassMapper/Interfaces/SortableModelInterface.php
+++ src/ClassMapper/Interfaces/SortableModelInterface.php
@@ -4,6 +4,10 @@ namespace Symphony\ClassMapper\ClassMapper\Interfaces;
 
 interface SortableModelInterface
 {
-    const SORT_DIRECTION_ASC = "asc";
-    const SORT_DIRECTION_DESC = "desc";
+    public const SORT_DIRECTION_ASC = "asc";
+    public const SORT_DIRECTION_DESC = "desc";
+
+    public const FLAG_SORTBY = 0x0100;
+    public const FLAG_SORTDESC = 0x200;
+    public const FLAG_SORTASC = 0x0400;
 }
