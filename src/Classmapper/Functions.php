0e896bff26daef15cf1d98ca22b8732d83b34aa9
Alannah Kearney <hi@alannahkearney.com>
2019-06-05 22:41:36 +1000

    Using Factory\ClassRegistry in Classmapper\create()

--- src/Classmapper/Functions.php
+++ src/Classmapper/Functions.php
@@ -4,63 +4,63 @@ declare(strict_types=1);
 
 namespace pointybeard\Symphony\Classmapper;
 
-function create(string $alias, string $sectionHandle): string
-{
-    if (class_exists($alias)) {
-        throw new \Exception(sprintf('Unable to create model. Returned: Class %s already exists in global scope', $alias));
+use pointybeard\Helpers\Functions\Strings;
+use pointybeard\Helpers\Foundation\Factory;
+
+if (!function_exists(__NAMESPACE__.'\create')) {
+    function create(string $alias, string $sectionHandle): string
+    {
+        if (class_exists($alias)) {
+            throw new \Exception(sprintf('Unable to create model. Returned: Class %s already exists in global scope', $alias));
+        }
+
+        $namespace = __NAMESPACE__;
+        $wrapperShortName = Strings\random_unique_classname('modelCreationWrapper', $namespace);
+        $wrapper = "{$namespace}\\{$wrapperShortName}";
+
+        /*
+         * Unfortunately, resorting to using eval() is necessary here. Anonymous
+         * classes will return the same instance if the wrapping class/function
+         * is the same. i.e. this function will ALWAYS return the exact same
+         * instance of the anonymous class. This is a huge problem since each
+         * model class needs to know which section it relates to.
+         *
+         * To solve this, the wrapper class name is dynamically generated to
+         * ensure a new instance of the anonymous class is always returned.
+         *
+         * The use of eval() does not get any user generated content
+         * so it is not possible for malicious user code to be executed. The use
+         * of Factory\ClassRegistry keeps the provided $sectionHandle value
+         * silo'd away so they cannot modify the resultant class
+         */
+        eval(sprintf(
+            'namespace %s;
+            use pointybeard\Helpers\Foundation\Factory;
+            final class %s {
+                public static function generate() {
+                    return get_class(new class() extends AbstractModel implements Interfaces\FilterableModelInterface {
+                        use Traits\hasModelTrait;
+                        use Traits\HasFilterableModelTrait;
+                        public function getSectionHandle(): ?string
+                        {
+                            [$sectionHandle] = Factory\ClassRegistry::lookup(self::class);
+                            return $sectionHandle;
+                        }
+                    });
+                }
+            }',
+            ltrim($namespace, '\\'),
+            $wrapperShortName,
+            $sectionHandle
+        ));
+
+        $classname = $wrapper::generate();
+        class_alias($classname, $alias);
+        Factory\ClassRegistry::register(
+            $classname,
+            $sectionHandle
+        );
+
+        return $alias;
     }
-
-    $randomString = function ($length = 32) {
-        return substr(hash('sha512', time().microtime()), 0, $length);
-    };
-
-    $randomClassName = function (string $prefix = null, string $namespace = null) use ($randomString) {
-        do {
-            $classname = $prefix.$randomString();
-        } while (class_exists("{$namespace}\\{$classname}"));
-
-        return $classname;
-    };
-
-    $namespace = '\\Symphony\\Classmapper';
-    $wrapperShortName = $randomClassName('modelCreationWrapper', $namespace);
-    $wrapper = "{$namespace}\\{$wrapperShortName}";
-
-    /*
-     * Unfortunately, resorting to using eval() is necessary here. Anonymous
-     * classes will return the same instance if the wrapping class/function
-     * is the same. i.e. this function will ALWAYS return the exact same
-     * instance of the anonymous class. This is a huge problem since
-     * self::class, used in symphonySectionHandle(), will always be the same
-     * no matter how many runtime model classes are created. The
-     * ModelRegistry::lookup() will only ever return the section handle for
-     * the most recently created class.
-     *
-     * To solve this, the wrapper class name is dynamically generated to ensure
-     * a new instance of the anonymous class is always returned.
-     *
-     * The use of eval() does not get any user generated content
-     * so it is not possible for malicious user code to be executed.
-     */
-    eval(sprintf(
-        '
-        namespace %s;
-        final class %s {
-            public static function generate() {
-                return get_class(new class() extends AbstractModel implements Interfaces\FilterableModelInterface {
-                    use Traits\hasModelTrait;
-                    use Traits\HasFilterableModelTrait;
-                    public const SECTION = \'%s\';
-                });
-            }
-        }',
-        ltrim($namespace, '\\'),
-        $wrapperShortName,
-        $sectionHandle
-    ));
-
-    $classname = $wrapper::generate();
-    class_alias($classname, $alias);
-
-    return $classname;
 }
