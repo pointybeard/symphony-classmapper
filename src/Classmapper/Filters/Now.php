8a730c0f77fa21409399a91d59167a8275b3c512
Alannah Kearney <hi@alannahkearney.com>
2019-06-05 22:39:15 +1000

    Passing type to parent constructor in Filters\Now::__construct()

--- src/Classmapper/Filters/Now.php
+++ src/Classmapper/Filters/Now.php
@@ -8,7 +8,7 @@ class Now extends Basic
 {
     public function __construct(string $field, string $comparisonOperator = self::COMPARISON_OPERATOR_EQ, string $operator = self::OPERATOR_AND)
     {
-        parent::__construct($field, null, $comparisonOperator, $operator);
+        parent::__construct($field, null, \PDO::PARAM_STR, $comparisonOperator, $operator);
     }
 
     protected function pattern(): string
