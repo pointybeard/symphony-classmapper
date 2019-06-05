0e6663092e0f5096e0dfdddce1923229df1e4388
Alannah Kearney <hi@alannahkearney.com>
2019-06-05 05:30:33 +0000

    Calling child class toArray() method rather than using self::

--- src/SectionClassMapper/AbstractFilter.php
+++ src/SectionClassMapper/AbstractFilter.php
@@ -75,7 +75,7 @@ abstract class AbstractFilter implements Interfaces\FilterInterface
 
     public function __toString(): string
     {
-        return json_encode($this->toArray(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        return json_encode(static::toArray(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     }
 
     public function getPattern(?int $flags = null): string
